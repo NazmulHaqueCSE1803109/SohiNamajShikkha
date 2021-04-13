7ee6dfb95fb9ea05eff8e7e63341c0e30ae49875
NazmulHaqueCSE1803109 <nazmulhaque1803109@gmail.com>
2021-04-14 00:33:45 +0600

    Updated about info

--- app/src/main/java/com/example/salat/SalatTime.java
+++ app/src/main/java/com/example/salat/SalatTime.java
@@ -175,7 +175,7 @@ public class SalatTime extends AppCompatActivity {
             Intent intent=new Intent(Intent.ACTION_SEND);
             intent.setType("text/plain");
             String subject="সহীহ নামাজ শিক্ষা";
-            String body="এটা একটা ইসলামিক অ্যাপ। অ্যাপটিতে ইসলামের মূল ভিত্তিগুলো সম্পর্কে মৌলিক ধারনা দেওয়া হয়েছে । এর মধ্যে গুরুত্বপূর্ণ বিষয় হলঃ কালেমা, নামাজ, রোজা, হজ্জ, জাকাত, কোরআন, হাদিস, নামাজের সময় হিসাব করা,যাকাত হিসাব করা, তাসবীহ হিসাব করা সহ আরও অনেক কিছু।";
+            String body="এটা একটা ইসলামিক অ্যাপ। অ্যাপটিতে ইসলামের মূল ভিত্তিগুলো সম্পর্কে মৌলিক ধারনা দেওয়া হয়েছে । এর মধ্যে গুরুত্বপূর্ণ বিষয় হলঃ কালেমা, নামাজ, রোজা, হজ্জ, জাকাত, কোরআন, হাদিস, নামাজের সময় হিসাব করা,যাকাত হিসাব করা, তাসবীহ হিসাব করা সহ আরও অনেক কিছু। https://github.com/NazmulHaqueCSE1803109/SohiNamajShikkha/raw/main/app-release.apk";
 
             intent.putExtra(Intent.EXTRA_SUBJECT,subject);
             intent.putExtra(Intent.EXTRA_TEXT,body);
@@ -190,12 +190,12 @@ public class SalatTime extends AppCompatActivity {
         else if(item.getItemId()==R.id.moreid)
         {
 
-            Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.jituhasan.namaz_shikkha"));
+            Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/NazmulHaqueCSE1803109/SohiNamajShikkha/raw/main/app-release.apk"));
             startActivity(browserIntent);
         }
         else if(item.getItemId()==R.id.rateid)
         {
-            Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.sevenonelab.bangla_namaz_shikkha"));
+            Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/NazmulHaqueCSE1803109/SohiNamajShikkha/raw/main/app-release.apk"));
             startActivity(browserIntent);
         }
         else if(item.getItemId()==R.id.appid)
