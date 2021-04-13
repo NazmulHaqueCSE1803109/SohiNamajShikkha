51a87a3a777e21f124dec05dd618b1bb7bb955be
NazmulHaqueCSE1803109 <nazmulhaque1803109@gmail.com>
2021-04-14 01:41:15 +0600

    Updated about info

--- app/src/main/java/com/example/salat/AboutApp.java
+++ app/src/main/java/com/example/salat/AboutApp.java
@@ -1,8 +1,15 @@
 package com.example.salat;
 
 //import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class AboutApp extends AppCompatActivity {
 
@@ -11,4 +18,58 @@ public class AboutApp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
     }
+
+
+    //menu items...
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.shareid)
+        {
+            Intent intent=new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            String subject="সহীহ নামাজ শিক্ষা";
+            String body="এটা একটা ইসলামিক অ্যাপ। অ্যাপটিতে ইসলামের মূল ভিত্তিগুলো সম্পর্কে মৌলিক ধারনা দেওয়া হইছে। এর মধ্যে গুরুত্বপূর্ণ বিষয় হলঃ কালেমা, নামাজ, রোজা, হজ্জ, জাকাত, কোরআন ও হাদিস শিক্ষা, নামাজের সময় হিসাব করা,যাকাত হিসাব করা, তাসবীহ হিসাব করা সহ আরও অনেক কিছু। More info: https://github.com/NazmulHaqueCSE1803109/SohiNamajShikkha";
+
+            intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+            intent.putExtra(Intent.EXTRA_TEXT,body);
+
+            startActivity(Intent.createChooser(intent,"share with"));
+        }
+        else if(item.getItemId()==R.id.feedbackid)
+        {
+            Intent intent=new Intent(this,Feedback.class);
+            startActivity(intent);
+        }
+        else if(item.getItemId()==R.id.moreid)
+        {
+
+            Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/NazmulHaqueCSE1803109/SohiNamajShikkha/raw/main/app-release.apk"));
+            startActivity(browserIntent);
+        }
+        else if(item.getItemId()==R.id.rateid)
+        {
+            Intent browserIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/NazmulHaqueCSE1803109/SohiNamajShikkha/raw/main/app-release.apk"));
+            startActivity(browserIntent);
+        }
+        else if(item.getItemId()==R.id.appid)
+        {
+            Intent intent=new Intent(this,AboutApp.class);
+            startActivity(intent);
+        }
+        else if(item.getItemId()==R.id.inventorid)
+        {
+            Intent intent=new Intent(this,AboutInventor.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
